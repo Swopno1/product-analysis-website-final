fcf67717790acb2a4a05adaecfd0aa998a2026b8
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-04 12:54:33 +0600

    Finish Reviews page.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({ review }) => {
+  const { img, post, rating, user } = review;
+
+  return (
+    <blockquote className="border rounded-xl shadow h-64 p-3">
+      <div className="flex items-center pb-3">
+        <img className="w-12 h-12 rounded-full" src={img} alt="user image" />
+        <div className="ml-3">
+          <cite className="text-lg font-semibold text-cyan-800">{user}</cite>
+          <p className="text-amber-600">Rating: {rating.toFixed(2)}</p>
+        </div>
+      </div>
+      <p className="leading-loose">
+        <span className="text-amber-500 font-bold pr-2 text-2xl">"</span>
+        {post}
+        <span className="text-amber-500 font-bold pr-2 text-2xl relative bottom-0 right-0">
+          "
+        </span>
+      </p>
+    </blockquote>
+  );
+};
+
+export default Card;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
+import Card from "../Card/Card";
 
 const Reviews = () => {
-  const [user, setUser] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     fetch("reviews.json")
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setReviews(data));
   }, []);
 
   return (
-    <div>
-      <h1>Reviews {console.log(user)}</h1>
+    <div className="container mx-auto">
+      <h1 className="text-3xl text-cyan-900 font-semibold text-center p-6">
+        User's Reviews: {reviews.length}
+      </h1>
+      <div className="md:grid grid-cols-3 gap-6">
+        {reviews.map((review) => (
+          <Card key={review.id} review={review}></Card>
+        ))}
+      </div>
     </div>
   );
 };
