e99a683674107ac908bfc00dc92553a5e768399d
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-04 23:53:58 +0600

    All update finished. Also edit the Readme.md file.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,18 @@
 import React from "react";
+import Article from "../Article/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "About Us",
+    text: "Birds-eye Review is a place to share and find user reviews about camera drones. You can find authentic expert users reviews about the camera drones. This is a open-source platform where users can submit there real experience with different type of drones.",
+  },
+  {
+    id: 2,
+    title: "Contact Us",
+    text: "Currently we don't allow our user to contact us directly. We don't share any contact information. All you can send us a query mail to info@xyz.com and we'll communicate to you if necessary.",
+  },
+];
 
 const About = () => {
   return (
@@ -6,6 +20,9 @@ const About = () => {
       <h2 className="text-3xl text-cyan-900 font-semibold text-center p-6">
         About
       </h2>
+      {articles.map((blog) => (
+        <Article key={blog.id} title={blog.title} text={blog.text}></Article>
+      ))}
     </div>
   );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>The page you're looking for is not found!</h2>
-      <h4>404</h4>
+    <div className="container mx-auto">
+      <h2 className="text-3xl text-cyan-900 font-semibold text-center px-6 py-12 my-12">
+        The page you're looking for is not found!
+      </h2>
+      <h4 className="text-center text-7xl text-amber-500 font-black">404</h4>
     </div>
   );
 };
